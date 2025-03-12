092cce4e58206c3ce80ae8f12e516f1fa1894180
Ahad Aljaloud <aaljaloudd@gmail.com>
2025-03-12 11:16:17 +0300

    withdraw method worked successfully!!

--- banking.py
+++ banking.py
@@ -11,14 +11,14 @@ def convert_acct_balance_info(info):
 class Bank:
     
     banking_info = [
-        { 'account_id': '10001',  'frst_name': 'suresh',  'last_name': 'sigera' , 'password':'juagw362',    'balance_checking':1000, 'balance_savings': 10000},
-        { 'account_id': '10002', 'frst_name': 'james', 'last_name': 'taylor' , 'password':'idh36%@#FGd', 'balance_checking': 10000, 'balance_savings':10000}, 
-        { 'account_id': '10003',  'frst_name': 'melvin', 'last_name': 'gordon' ,'password':'uYWE732g4ga1',    'balance_checking':2000, 'balance_savings':20000}, 
-        { 'account_id': '10004', 'frst_name': 'stacey', 'last_name': 'abrams' ,'password':'DEU8_qw3y72$',    'balance_checking':2000, 'balance_savings':20000}, 
-        { 'account_id': '10005',  'frst_name': 'jake', 'last_name': 'paul', 'password':'d^dg23g)@','balance_checking':100000,'balance_savings':100000}
+        { 'account_id': '10001',  'frst_name': 'suresh',  'last_name': 'sigera' , 'password':'juagw362',    'balance_checking':1000, 'balance_savings': 10000 , 'account_status':'active' , 'count_overdraft':0 },
+        { 'account_id': '10002', 'frst_name': 'james', 'last_name': 'taylor' , 'password':'idh36%@#FGd', 'balance_checking': 10000, 'balance_savings':10000 , 'account_status':'active' , 'count_overdraft':0 }, 
+        { 'account_id': '10003',  'frst_name': 'melvin', 'last_name': 'gordon' ,'password':'uYWE732g4ga1',    'balance_checking':2000, 'balance_savings':20000 , 'account_status':'active' , 'count_overdraft':0 }, 
+        { 'account_id': '10004', 'frst_name': 'stacey', 'last_name': 'abrams' ,'password':'DEU8_qw3y72$',    'balance_checking':2000, 'balance_savings':20000 , 'account_status':'active' , 'count_overdraft':0 }, 
+        { 'account_id': '10005',  'frst_name': 'jake', 'last_name': 'paul', 'password':'d^dg23g)@','balance_checking':100000,'balance_savings':100000, 'account_status':'active' , 'count_overdraft':0 }
     ]
         
-    fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings"]
+    fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings","account_status","count_overdraft"]
         
         
     if not os.path.exists("./bank.csv"):
@@ -50,10 +50,10 @@ class Customer:
         last_account = last_account.split(",")
         last_account_id = int(last_account[0])
         self.account_id = str(last_account_id + 1)
-        self.fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings"]
+        self.fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings","account_status","count_overdraft"]
 
     def new_customer(self):
-        
+
         try:
             user_response=int(input("Hello! Welcome to the Bank\nChoose 1 or 2:\n1- Log in\n2- Create an account\n"))
             if user_response == 1:
@@ -87,7 +87,7 @@ class Customer:
                     except ValueError:
                         print("Invalid input!! Please enter a vali  number.") 
 
-                    new_user = { 'account_id':self.account_id, 'frst_name': first_name,  'last_name': last_name ,'password':password, 'balance_checking':balance_checking,'balance_savings':balance_savings} 
+                    new_user = { 'account_id':self.account_id, 'frst_name': first_name,  'last_name': last_name ,'password':password, 'balance_checking':balance_checking,'balance_savings':balance_savings, 'account_status':'active','count_overdraft':0} 
                     try:
                         with open("bank.csv", "a+") as csvfile:
                             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
@@ -111,7 +111,10 @@ class Account:
         self.last_name = None
         self.password = None
         self.balance_checking = None
-        self.balance_savings = None 
+        self.balance_savings = None
+        self.account_status = 'active'
+        self.count_overdraft = 0
+        
         
      
     def log_in(self):
@@ -129,6 +132,7 @@ class Account:
                         self.password = col["password"]
                         self.balance_checking = convert_acct_balance_info(col["balance_checking"])
                         self.balance_savings = convert_acct_balance_info(col["balance_savings"])
+                        self.account_status = col.get("account_status", "Active")
                         check_acct_num = False
                         break
                 if check_acct_num:
@@ -136,6 +140,7 @@ class Account:
             check_password = True
             while check_password:
                 user_password = input("Enter your password: ")
+
                 if user_password == self.password:
                     print(f"Hello {self.first_name} {self.last_name} with the following acount id {self.account_id} !\nYour account checking balance is {self.balance_checking}\nYour saving balance is {self.balance_savings} .")
                     check_password=False
@@ -174,27 +179,36 @@ class Operation:
     def __init__(self, account):
 
         self.account = account
-        self.fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings"]
+        self.fieldnames = ["account_id", "frst_name", "last_name", "password", "balance_checking", "balance_savings","account_status","count_overdraft"]
        
 
     def withdraw(self):
 
         try:
+            if self.account.account_status == "deactivated":
+                print("Your account has been deactivated due to overdrafts. Please deposit enough money to reactivate it. ")
+                return
+
             user_withdraw=int(input("Do you want to withdraw from savings or checking balance?\n1- withdraw from saving balance\n2- withdraw from checking balance\n"))
             if user_withdraw == 1 or user_withdraw == 2:
                 user_withdraw_amount=float(input("How much money do you want to withdraw? "))
     
                 if user_withdraw == 1 and self.account.balance_savings is not None:
                     if user_withdraw_amount <= 100:
+
                         if self.account.balance_savings > 0:
                             self.account.balance_savings -= user_withdraw_amount
                             print(f"Withdraw {user_withdraw_amount} from saving. Your new saving balance now is {self.account.balance_savings} .")
-                            self.update_new_balance()
+
                         else:
                             self.account.balance_savings -= user_withdraw_amount
                             self.account.balance_savings -= 35
-                            print(f"Withdraw {user_withdraw_amount} from saving.Your acount balance is negative you will get a fee of 35. Your new saving balance now is {self.account.balance_savings} .")
-                            self.update_new_balance()
+                            print(f"Withdraw {user_withdraw_amount} from saving.Your acount balance is negative you will get a fee of 35. Your savings account is overdrafted.\nYour new saving balance now is {self.account.balance_savings} .")
+
+                            self.account.count_overdraft += 1
+                            if self.account.count_overdraft > 2:
+                               self.account.account_status = "deactivated"
+                               print("Your account has been deactivated due to multiple overdrafts.") 
 
                     else:
                         print("You can't withdraw more that 100")
@@ -202,19 +216,27 @@ class Operation:
 
                 elif user_withdraw == 2 and self.account.balance_checking is not None:
                     if user_withdraw_amount <= 100:
+
                         if self.account.balance_checking > 0:
                             self.account.balance_checking -= user_withdraw_amount
                             print(f"Withdraw {user_withdraw_amount} from checking. Your new checking balance now is {self.account.balance_checking} .")
-                            self.update_new_balance()
+
                         else:
                             self.account.balance_checking -= user_withdraw_amount
                             self.account.balance_checking -= 35
-                            print(f"Withdraw {user_withdraw_amount} from checking.Your acount balance is negative you will get a fee of 35. Your new checking balance now is {self.account.balance_checking} .")
-                            self.update_new_balance()
+                            print(f"Withdraw {user_withdraw_amount} from checking.Your acount balance is negative you will get a fee of 35. Your checking account is overdrafted.\nYour new checking balance now is {self.account.balance_checking} .")
+
+                            self.account.count_overdraft += 1
+                            if self.account.count_overdraft > 2:
+                               self.account.account_status = "deactivated"
+                               print("Your account has been deactivated due to multiple overdrafts.") 
                     else:
                         print("You can't withdraw more that 100")
                 else:
                     print("The user does not have an acount")
+
+                self.update_new_balance()
+
             else:
                 print("Invalid input choose 1 or 2")
        
@@ -225,6 +247,7 @@ class Operation:
     def deposite(self):
 
         try:
+
             user_deposite=int(input("Do you want to deposite money to savings or checking balance?\n1- deposite to saving balance\n2- deposite to checking balance\n"))
 
             if user_deposite == 1 or user_deposite == 2:
@@ -233,19 +256,37 @@ class Operation:
                 if user_deposite == 1 and self.account.balance_savings is not None:
                     self.account.balance_savings += user_deposite_amount
                     print(f"Deposite {user_deposite_amount} to saving. Your new saving balance now is {self.account.balance_savings} .")
-                    self.update_new_balance()
+
+                    if self.account.account_status == "deactivated" and self.account.balance_savings >0:
+                        self.account.account_status = "active"
+                        self.count_overdraft = 0
+                        print("Your account has been reactivated!")
+
+
                 elif user_deposite == 2 and self.account.balance_checking is not None:
                     self.account.balance_checking += user_deposite_amount
                     print(f"Deposite {user_deposite_amount} to checking. Your new checking balance now is {self.account.balance_checking} .")
-                    self.update_new_balance()
+
+                    if self.account.account_status == "deactivated" and self.account.balance_checking >0:
+                        self.account.account_status = "active"
+                        self.count_overdraft = 0
+                        print("Your account has been reactivated!")
+                    
                 else:
                     print("Invalid account type!")
+                    
+                self.update_new_balance()
             else:
                 print("Invalid number! Please enter 1 or 2")
         except ValueError:
                     print("Invalid input!! Please enter a valid number.")
+
     
     def transfer(self):
+
+        if self.account.account_status == "deactivated":
+                print("Your account has been deactivated due to overdrafts. Please deposit enough money to reactivate it. ")
+                return
 
         user_transfer = input("Enter the acount id you want to deposite to: ")
         if self.account.account_id == user_transfer:
@@ -274,6 +315,16 @@ class Operation:
 
             except ValueError:
                     print("Invalid input!! Please enter a valid number.")
+        # else:
+        #     try:
+        #         with open("bank.csv", "r") as bank:
+        #         data = csv.DictReader(bank)
+        #         for col in data:
+        #             if col["account_id"] == user_transfer:
+        #                 self.balance_checking = convert_acct_balance_info(col["balance_checking"])
+        #                 self.balance_savings = convert_acct_balance_info(col["balance_savings"])
+        #                 break
+        
 
             
             
@@ -287,11 +338,13 @@ class Operation:
                 if col["account_id"] == self.account.account_id:
                     col["balance_savings"] = str(self.account.balance_savings)
                     col["balance_checking"] = str(self.account.balance_checking)
+                    col["account_status"] = self.account.account_status 
+                    col["count_overdraft"] = self.account.count_overdraft
                 updated.append(col)
 
         with open("./bank.csv", 'w', newline='') as csvfile:
             try:
-                writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+                writer = csv.DictWriter(csvfile, fieldnames = self.fieldnames)
                 writer.writeheader()
                 for row in updated:
                     writer.writerow(row)
